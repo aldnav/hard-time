43f31c3c7fa7bbd9b0ae723637bd4714c5e68a8d
Aldrin Navarro <aldrinnavarro16@gmail.com>
2014-09-28 22:42:49 +0800

    Added extraction functions for initial, boolean, increment and body statements

--- Lexer.java
+++ Lexer.java
@@ -23,14 +23,14 @@ import java.util.logging.Logger;
  * @author aldnav
  */
 public class Lexer {
-    
+
     Parser parser;
     Path filePath;
     List<String> lines;
     List<ForLoop> loops;
     double loopCounter;
-    
-    public Lexer (Path inputFilePath) {
+
+    public Lexer(Path inputFilePath) {
         parser = new Parser();
         filePath = inputFilePath;
         Charset charset = Charset.forName("UTF-8");
@@ -42,72 +42,103 @@ public class Lexer {
             Logger.getLogger(Lexer.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     public void analyze() {
         // identify the loops
 //        Stack loopStack = new Stack();
-        ForLoop alphaLoop;
+        ForLoop alphaLoop = null;
 //        Boolean foundLoopFlag = false;
         Iterator lineIter = lines.iterator();
-        
+        Boolean checkTokens = true;
+        int lineCtr = 0;
+
         while (lineIter.hasNext()) {
             String line = lineIter.next().toString();
             ArrayList<Parser.Token> tokens = parser.parse(line);
-            for (Parser.Token token: tokens) {
-                if (token.type == Parser.TokenType.FOR) {
-                    alphaLoop = new ForLoop(loopCounter);                    
-                    alphaLoop.initialStatement = extractStatement(line, 0);
-                    alphaLoop.booleanExpression = extractStatement(line, 1);
-                    alphaLoop.incrementStatement = extractStatement(line, 2);
-                    loops.add(alphaLoop);
-                    loopCounter += 1.0;
+            checkTokens = line.contains("for");
+            if (checkTokens) {
+                for (Parser.Token token : tokens) {
+                    if (token.type == Parser.TokenType.FOR) {
+                        alphaLoop = new ForLoop(loopCounter);
+                        alphaLoop.initialStatement = extractStatement(line, 0);
+                        alphaLoop.booleanExpression = extractStatement(line, 1);
+                        alphaLoop.incrementStatement = extractStatement(line, 2);
+                        alphaLoop.bodyStatements = extractBodyStatement(lines.subList(lineCtr+1, lines.lastIndexOf("}")));
+                        loops.add(alphaLoop);
+                        loopCounter += 1.0;
+                        checkTokens = false; // might want to change this
+                    }
+                    if (!checkTokens) {
+                        break;
+                    }
                 }
             }
+            lineCtr++;
         }
-        
+
         // Display for loops
-        for (ForLoop loop: loops)
-            System.out.println(loop);        
+        for (ForLoop loop : loops) {
+            System.out.println(loop);
+        }
     }
-    
+
     private String extractStatement(String line, int order) {
         // order
         // 0 - initial statement
         // 1 - boolean expression
         // 2 - increment statement
-        if (order > 2)
+        if (order > 2) {
             return "";
+        }
         return line.split(";")[order].replace("for(", "").replace("){", "");
     }
-    
+
+    private List extractBodyStatement(List<String> lines) {
+        List contents = new ArrayList();
+        for (String line: lines) {
+            if (line.equals(("}")))
+                break;
+            contents.add(line);
+        }        
+        return contents;
+    }
+
     public class ForLoop {
+
         double id = 0.0;
         String initialStatement = "";
         String booleanExpression = "";
         String incrementStatement = "";
-        String bodyStatements = "";
+        List bodyStatements;
         List<ForLoop> innerLoops;
-        
+
         public ForLoop() {
             innerLoops = new ArrayList<ForLoop>();
+            bodyStatements = new ArrayList();
         }
-        
+
         public ForLoop(double _id) {
             id = _id;
             innerLoops = new ArrayList<ForLoop>();
+            bodyStatements = new ArrayList();
         }
 
         @Override
         public String toString() {
-            return "ForLoop{" + "id=" + id + ", initialStatement=" + initialStatement + ", booleanExpression=" + booleanExpression + ", incrementStatement=" + incrementStatement + ", bodyStatements=" + bodyStatements + ", innerLoops=" + innerLoops + '}';
+            return "ForLoop{" + "id=" + id + ",\n"
+                    + "\tinitialStatement=" + initialStatement + ",\n"
+                    + "\tbooleanExpression=" + booleanExpression + ",\n"
+                    + "\tincrementStatement=" + incrementStatement + ",\n "
+                    + "\tbodyStatements=" + bodyStatements + ",\n"
+                    + "\tinnerLoops=" + innerLoops + '}';
         }
-        
+
     }
-    
+
     public static void main(String[] args) {
         Path filePath = Paths.get(System.getProperty("user.dir") + "/src/mp2", "input.txt");
         Lexer lexer = new Lexer(filePath);
         lexer.analyze();
     }
-    
+
 }
